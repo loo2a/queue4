178cec158f7250061b5ebb1924082bd85cf57c19
loo2a <lu2adeveloper@gmail.com>
2025-11-22 15:50:11 +0200

    Update firebase-config.js

--- firebase-config.js
+++ firebase-config.js
@@ -1,14 +1,16 @@
 // تهيئة Firebase
-const firebaseConfig = {
-    apiKey: "YOUR_API_KEY",
-    authDomain: "YOUR_PROJECT.firebaseapp.com",
-    databaseURL: "https://YOUR_PROJECT-default-rtdb.firebaseio.com",
-    projectId: "YOUR_PROJECT",
-    storageBucket: "YOUR_PROJECT.appspot.com",
-    messagingSenderId: "YOUR_SENDER_ID",
-    appId: "YOUR_APP_ID"
-};
 
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyAqisKc23O4jK3icohi1gJxBT8PutUOYFQ",
+  authDomain: "queue7-c107b.firebaseapp.com",
+  databaseURL: "https://queue7-c107b-default-rtdb.firebaseio.com",
+  projectId: "queue7-c107b",
+  storageBucket: "queue7-c107b.firebasestorage.app",
+  messagingSenderId: "143343650250",
+  appId: "1:143343650250:web:97da71efabf44309963b33",
+  measurementId: "G-9TP4P04413"
+};
 firebase.initializeApp(firebaseConfig);
 const database = firebase.database();
 
